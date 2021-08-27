013971a8482783ebb8a15c128e235e8bff86b1f8
victor.rivera.lc <victor.rivera@lcliquidezcorporativa.com.mx>
2021-08-27 12:49:17 -0700

    Terminado

--- Descuentos/descuentos.js
+++ Descuentos/descuentos.js
@@ -18,6 +18,105 @@ function onClickButtonPriceDiscount(){
     resultP.innerText = "El precio con descuento son $"+precioConDescuento+" USD";
 }
 
+const coupons1 = [
+    "Cupón Super Descuento",
+    "Cupón Ultra Descuento",
+    "Cupón Mega Descuento"
+]
+
+const coupons2 = [ {
+        name: "Cupón Super Descuento",
+        discount: 15,
+    },
+    {
+        name: "Cupón Ultra Descuento",
+        discount: 20,
+    },
+    {
+    name: "Cupón Mega Descuento",
+    discount: 30,
+    }
+];
+
+function onClickButtonPriceCoupon1(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+    
+    let descuento;
+
+    switch(couponValue) {
+        case "Cupón Super Descuento":
+            descuento = 15;
+            break;
+        case "Cupón Ultra Descuento":
+            descuento = 20;
+            break;
+        case "Cupón Mega Descuento":
+            descuento = 30;
+            break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    const resultP = document.getElementById("ResultPrice")
+    resultP.innerText = "El precio con descuento son $"+precioConDescuento+" USD";
+}
+
+function onClickButtonPriceCoupon2(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+    
+    if(!coupons1.includes(couponValue)){
+        alert("El cupón \""+couponValue+"\" no existe, favor de revisar la redacción del texto.")
+        const resultP = document.getElementById("ResultPrice")
+        resultP.innerText = "";
+    } else {
+        let descuento;
+        if (couponValue === "Cupón Super Descuento") {
+            descuento = 15;
+         } else if (couponValue === "Cupón Ultra Descuento") {
+            descuento = 20;
+         } else if (couponValue === "Cupón Mega Descuento") {
+            descuento = 30;
+         }
+
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+        const resultP = document.getElementById("ResultPrice")
+        resultP.innerText = "El precio con descuento son $"+precioConDescuento+" USD";
+    }
+}
+
+
+function onClickButtonPriceCoupon3(){
+    debugger;
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+    
+    const isCouponValueValid = function(coupon){
+        return coupon.name === couponValue;
+    };
+    
+    const userCoupon = coupons2.find(isCouponValueValid)
+
+    if(!userCoupon){
+        alert("El cupón \""+couponValue+"\" no existe, favor de revisar la redacción del texto.")
+        const resultP = document.getElementById("ResultPrice")
+        resultP.innerText = "";
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+        const resultP = document.getElementById("ResultPrice")
+        resultP.innerText = "El precio con descuento son $"+precioConDescuento+" USD";
+    }
+}
 /*console.log({
     precioOriginal,
     descuento,
---
+++ s
@@ -0,0 +1,35 @@
+[33mcommit 106a0e6bb7706ad5e02df5467d5a0ebd3bbd5f98[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: victor.rivera.lc <victor.rivera@lcliquidezcorporativa.com.mx>
+Date:   Fri Aug 27 10:00:15 2021 -0700
+
+    Funcional
+
+[33mcommit 583c14f486fef56ab8381604af223ba4d208042f[m
+Author: victor.rivera.lc <victor.rivera@lcliquidezcorporativa.com.mx>
+Date:   Fri Aug 27 09:10:33 2021 -0700
+
+    Creado carpetas
+
+[33mcommit e5cb79ca9633c7637cb04bcaf3c8fd8377b77166[m
+Author: victor.rivera.lc <victor.rivera@lcliquidezcorporativa.com.mx>
+Date:   Fri Aug 27 09:07:03 2021 -0700
+
+    Compleado el ejercicio
+
+[33mcommit 992c17f89e66ea86add4ac142c1d0e1219ddfc1a[m
+Author: victor.rivera.lc <victor.rivera@lcliquidezcorporativa.com.mx>
+Date:   Thu Aug 26 16:25:25 2021 -0700
+
+    Formulario con cuadrado
+
+[33mcommit 7979ad41ab4d1030bfbe0edee036bec6f2392279[m
+Author: victor.rivera.lc <victor.rivera@lcliquidezcorporativa.com.mx>
+Date:   Mon Aug 16 10:10:11 2021 -0700
+
+    Primer commit
+
+[33mcommit d50a9a88ed3d4ec39c8891dfee2020ff7d0eea32[m
+Author: victor-rivera-lc <88060399+victor-rivera-lc@users.noreply.github.com>
+Date:   Mon Aug 16 09:23:33 2021 -0700
+
+    Initial commit
